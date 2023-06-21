2eac82608194186f9a670f618bbd36ccac265fa1
nico858 <91767917+nico858@users.noreply.github.com>
2023-06-21 08:47:55 -0500

    Update exercise7.py

--- exercise7.py
+++ exercise7.py
@@ -16,25 +16,6 @@ def max_programas_recursive (programs, weights, discCapacity):
 
 print(f'Recursiva {max_programas_recursive(programs, weights, disc_capacity)}')
 
-def max_programs_iterative(programas, pesos, capacidad):
-    n = len(programas)
-    dp = [[0] * (capacidad + 1) for _ in range(n + 1)]
-
-    for i in range(n + 1):
-        for j in range(capacidad + 1):
-            if i == 0 or j == 0:
-                dp[i][j] = 0
-            elif pesos[i - 1] > j:
-                dp[i][j] = dp[i - 1][j]
-            else:
-                incluir = weights[i - 1] + dp[i - 1][j - pesos[i - 1]]
-                excluir = dp[i - 1][j]
-                dp[i][j] = max(incluir, excluir)
-
-    return dp[n][capacidad]
-
-print(f'Iterativa {max_programs_iterative(programs, weights, disc_capacity)}')
-
 
 def max_programs_memoized(programs, weights, disc_capacity):
     n = len(programs)
